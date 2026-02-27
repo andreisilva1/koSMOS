c2341c4f037f4a445dbbf59cf258aa2dd6269dc7
andrei <andrei.fbds@gmail.com>
2026-02-26 22:13:18 -0300

    type: refactor, scope: check_colinearity and analyze functions

--- backend/app.py
+++ backend/app.py
@@ -6,18 +6,18 @@ from fastapi import FastAPI, UploadFile
 import pandas as pd
 from pydantic import BaseModel
 
+from model_tests import check_colinearity
+
 app = FastAPI()
 
-class BaseFile(BaseModel):
-    file: UploadFile
-    id_column: Optional[str]
-
 @app.post("/analyze")
-async def analyze(dict_file: BaseFile):
+async def analyze(file: UploadFile, id_column: str = None):
     # File reader
-    contents = await dict_file.file.read()
-    df = pd.read_table(BytesIO(contents))
+    contents = await file.read()
+    df = pd.read_csv(BytesIO(contents))
     
-    # Delete id column if user says
-    if dict_file.id_column:
-        df = df.drop(columns=dict_file.id_column)
+    # Delete id_column
+    if id_column and id_column in df.columns[0].split(","):
+        df.drop(columns=id_column, inplace=True)
+      
+    return check_colinearity(df, "y")
--- backend/model_tests.py
+++ backend/model_tests.py
@@ -6,23 +6,8 @@ from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn.preprocessing import StandardScaler
 
 def check_colinearity(df: DataFrame, target: str):
-    # By Correlation Matriz
-    corr = df.corr(numeric_only=True)
-    pair_corr = []
-    cols = corr.columns
-    for i in range(len(cols)):
-        for j in range(i + 1, len(cols)):
-            pair_corr.append(cols[i], cols[j], cols.iloc[i, j])
-    
-    # 1 pair with more than 0.9 correlation or 10% or the pairs more than 0.85 correlation = strong colinearity, that makes sense... I think      
-    corr_more_than_90 = []
-    corr_more_than_90.append([f1, f2, val] for f1, f2, val in pair_corr if abs(pair_corr[2]) >= 0.9)
-    corr_more_than_85 = []
-    corr_more_than_85.append([f1, f2, val] for f1, f2, val in pair_corr if abs(pair_corr[2]) >= 0.85)
-
-    # Check colinearity by VIF
     X = df.drop(columns=target)
-    
+
     # Made a OneHot Encoder in all the str columns in the df
     str_columns = [X[col] for col in X.columns if is_string_dtype(X[col])]
     really_str_columns = []
@@ -36,6 +21,23 @@ def check_colinearity(df: DataFrame, target: str):
     
     df = pd.get_dummies(df, columns=really_str_columns, dtype="int64")
     
+    # By Correlation Matriz
+    corr = df.corr(numeric_only=True)
+    print(df.corr())
+    pair_corr = []
+    cols = corr.columns
+    for i in range(len(cols)):
+        for j in range(i + 1, len(cols)):
+            pair_corr.append({"col_1": cols[i], "col_2": cols[j], "corr": corr.iloc[i, j]})
+    
+    # 1 pair with more than 0.9 correlation or 10% or the pairs more than 0.85 correlation = strong colinearity, that makes sense... I think      
+    corr_more_than_90 = []
+    corr_more_than_85 = []
+
+    corr_more_than_90 = len([x for x in pair_corr if abs(x["corr"]) >= 0.9])
+    corr_more_than_85 = len([x for x in pair_corr if abs(x["corr"]) >= 0.85])
+
+    # Check colinearity by VIF    
     # VIF Verification (Variance Inflation Factor) -> How much a variable is been explained by the others
     vif_data = pd.DataFrame()
     vif_data["feature"] = X.columns
@@ -44,6 +46,6 @@ def check_colinearity(df: DataFrame, target: str):
     # Numeric stability of the X matriz - Detect when small variations in data can cause large changes in coefficients
     cond = np.linalg.cond(StandardScaler().fit_transform(X))
     
-    # Return true if one correlation > 0.9, 2 correlations > 0.85, VIF average >= 5, any VIF >= 10, cond > 100
-    return len(corr_more_than_90) >= 1 or len(corr_more_than_85) / len(pair_corr) * 100 > 0.1 or vif_data["VIF"] / len(vif_data) >= 5 or vif_data[vif_data["VIF"] >= 10] or cond > 100
+    # Return true if one correlation > 0.9, 2 correlations > 0.85, VIF average >= 5, any VIF >= 10 or cond > 100
+    return [bool(x) for x in [corr_more_than_90 >= 1, (corr_more_than_85 / len(pair_corr)) > 0.1, (sum(vif_data.VIF) / len(vif_data)) >= 5, len(vif_data[vif_data["VIF"] >= 10]) > 0, cond > 100]]
     
